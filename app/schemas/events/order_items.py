a7be78afb6081f5be625c64b9ece4353ea24451d
Akashkarthik612 <akashkarthik612@gmail.com>
2026-02-23 21:24:39 +0100

    updated test_file

---
+++ app/schemas/events/order_items.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
+from typing import Optional
+
+
+class OrderItemCreate(BaseModel):
+    order_id: str
+    product_id: str
+    description: Optional[str] = None
+    quantity: int = Field(..., gt=0)
+    price_at_purchase: int
+    event_time: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class OrderItemResponse(OrderItemCreate):
+    event_id: int
+
+    model_config = ConfigDict(from_attributes=True)
